377388eb8e99dd2818d33d98cbb27eb6b5b4b8ce
shishir1310 <shishirroy1310@gmail.com>
2019-08-03 18:46:45 +0600

    Update

---
+++ airtable/44. Wildcard Matching.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution1 {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        bool dp[n+1][m+1];
+        memset(dp,0,sizeof dp);
+        dp[0][0] = 1;
+        for(int i=1;i<=m;i++)
+        {
+            if(p[i-1]=='*')
+            {
+                dp[0][i] = dp[0][i-1];
+            }
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==p[j-1] or p[j-1]=='?')
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*')
+                {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else
+                {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        // for(int i=0;i<=n;i++)
+        // {
+        //     for(int j=0;j<=m;j++)
+        //     {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return dp[n][m];
+    }
+};
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        bool dp[2][m+1];
+        memset(dp,0,sizeof dp);
+        dp[0][0] = 1;
+        for(int i=1;i<=m;i++)
+        {
+            if(p[i-1]=='*')
+            {
+                dp[0][i] = dp[0][i-1];
+            }
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                int x = i&1;// odd
+                if(s[i-1]==p[j-1] or p[j-1]=='?')
+                {
+                    dp[x][j] = dp[x^1][j-1];
+                }
+                else if(p[j-1]=='*')
+                {
+                    dp[x][j] = dp[x^1][j] || dp[x][j-1];
+                }
+                else
+                {
+                    dp[x][j] = 0;
+                }
+            }
+        }
+        // for(int i=0;i<=n;i++)
+        // {
+        //     for(int j=0;j<=m;j++)
+        //     {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return dp[n&1][m];
+    }
+};
+int main()
+{
+    Solution t;
+    cout << t.isMatch("aa","*");
+    return 0;
+}
